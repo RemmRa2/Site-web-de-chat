8f712109646f5e60e6c64e886ee2d8642c375a20
RemmRa2 <rayan.remmouche2@ped.csmb.qc.ca>
2026-09-17 20:12:29 -0400

    Optimisation de la fonction writeFile

--- server.js
+++ server.js
@@ -1,45 +1,53 @@
 const express = require('express');
 const app = express();
-const {WebSocketServer} = require('ws');
 const http = require('http');
+const { WebSocketServer } = require('ws');
 const crypto = require('crypto');
 const fs = require('fs/promises');
-const {setTimeout} = require('timers/promises');
+const { EventEmitter, once } = require('events');
+
+const server = http.createServer(app);
+const wss = new WebSocketServer({ server });
+const eventBus = new EventEmitter();
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+const mapClients = new Map();
 
 let etatEcriture = Promise.resolve();
 let queueSize = 0;
-const server = http.createServer(app);
-const wss = new WebSocketServer({ server });
-app.use(express.static('public'));
-app.set('view engine', 'ejs');
-const mapClients = new Map()
-Map.prototype.find = function(callback)  {
-  for (const [clé, valeur] of this.entries()) {
-    if (callback(valeur, clé) === true) {
-      return { valeur, clé };
-    }
+
+async function writeFile(chemin, contenu, option = 'utf8') {
+  while (queueSize >= 500) {
+    await once(eventBus, 'placeLiberee');
   }
-  return null;
-}
-async function writeFile(chemin, contenu, option = 'utf8'){
-  while (queueSize >= 500){
-    await setTimeout(50);
-  }
+
   queueSize++;
-  const attente = etatEcriture.catch(()=>{});
-	etatEcriture = (async function(){
-		await attente; 
-		return await fs.writeFile(chemin, contenu, option);
-	})();
-	try{
-		return await etatEcriture;
-	}
-	finally{
-		queueSize--;
-	}
+
+  const attente = etatEcriture.catch(() => {});
+  etatEcriture = (async function () {
+    await attente;
+    return await fs.writeFile(chemin, contenu, option);
+  })();
+
+  try {
+    return await etatEcriture;
+  } finally {
+    queueSize--;
+    eventBus.emit('placeLiberee');
+  }
 }
+
 wss.on('connection', (socket) => {
   socket.id = crypto.randomUUID();
-	clientsMap.set(socket.id, socket);
+  mapClients.set(socket.id, socket);
+
+  socket.on('close', () => {
+    mapClients.delete(socket.id);
+  });
 });
-server.listen(3000, ()=>{})
+
+server.listen(3000, () => {
+  console.log('Serveur démarré sur http://localhost:3000');
+});
